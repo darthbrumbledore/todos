6fe66707dffe30c8c2e4b4580aa9db9176d38754
Carlton Brumbelow <carlton.brumbelow@Carltons-MBP-2.eau.jamf.net>
2018-05-25 15:10:25 -0500

    added update functionality

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+const _ = require('lodash');
 const express = require('express'); // express back end for routing and requests
 const bodyParser = require('body-parser'); // parses POST requests to JSON
 const {ObjectID} = require('mongodb');
+
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -75,9 +77,39 @@ app.delete('/todos/:id', (req, res) => {
     res.send(todo);
   }).catch((e) => {
     res.status(400).send();
-  })
+  });
 
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  //use lodash pick method to limit the fields users are allowed to update.
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(400).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 })
+
 
 // Server listener
 app.listen(port, () => {
